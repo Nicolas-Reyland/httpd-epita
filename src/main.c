8c75a72a024a86d1be63589885d32502ad7e292a
Nicolas Reyland <nicolas@reyland.dev>
2022-11-08 20:19:49 +0100

    fixed some memory errors

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#include "utils/hash_map/hash_map.h"
+#include "utils/parsers/config/config_parser.h"
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+        return 4;
+
+    struct server_config *config = parse_server_config(argv[1]);
+    if (config == NULL)
+    {
+        puts("(null)");
+        return 0;
+    }
+
+    puts("global :");
+    hash_map_dump(config->global);
+    for (size_t i = 0; i < config->num_vhosts; ++i)
+    {
+        printf("vhosts %zu\n", i);
+        hash_map_dump(config->vhosts[i]);
+    }
+
+    return 0;
+}

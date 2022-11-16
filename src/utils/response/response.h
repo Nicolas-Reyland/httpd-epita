9ed29026c47f6f72caace800025871e3b5dab422
Nicolas Reyland <nicolas@reyland.dev>
2022-11-16 22:18:27 +0100

    feat: added attribtue 'close_connection' in response struct

--- src/utils/response/response.h
+++ src/utils/response/response.h
@@ -1,19 +1,20 @@
 #ifndef RESPONSE_H
 #define RESPONSE_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <sys/types.h>
 
 #include "network/vhost.h"
 #include "utils/parsers/http/parser_request.h"
 
-struct response
-{
-    char *res;
-    size_t res_len;
-    int fd;
-    size_t file_len;
-    int err;
+struct response {
+  char *res;
+  size_t res_len;
+  int fd;
+  size_t file_len;
+  int err;
+  bool close_connection;
 };
 
 struct response *parsing_http(char *request_raw, size_t size,

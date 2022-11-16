11821dc1425945f871ab9ca96fda08a7b79f3763
Nicolas Reyland <nicolas.reyland@epita.fr>
2022-11-16 13:43:48 +0100

    feat: case-insensitive hash function

--- src/utils/hash_map/hash.c
+++ src/utils/hash_map/hash.c
@@ -6,7 +6,7 @@ size_t hash(char *key)
     size_t hash = 0;
 
     for (i = 0; key[i] != '\0'; ++i)
-        hash += key[i];
+        hash += 'A' <= key[i] && key[i] <= 'Z' ? key[i] - 'A' + 'a' : key[i];
     hash += i;
 
     return hash;

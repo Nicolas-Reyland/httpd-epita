0440fe0fe35fabbf143c5b3257ae67c5c771d83f
Antoine Riquet <antoine.riquet@epita.fr>
2022-11-17 19:04:09 +0100

    feat: push for dementor

--- src/utils/response/response.c
+++ src/utils/response/response.c
@@ -65,7 +65,7 @@ struct response *create_response(int *err, struct client *client,
         return set_error_response(client->vhost, resp, err);
     }
 
-    // Access ressource (file)
+    // Access ressources (file)
     int open_ressource_result = open_ressource(path, resp, client->vhost,
                                                strcmp(req->method, "GET") == 0);
     *err = resp->err;

f691b3e879a6872f1e3f7df0568a7988354a84c0
Nicolas Reyland <nicolas@reyland.dev>
2022-11-19 00:26:40 +0100

    fix: not using setsiid to daemonize, unless compile SETSID_DAEMONIZE flag set

--- src/process/daemonize.c
+++ src/process/daemonize.c
@@ -19,7 +19,8 @@ void daemonize(void)
     if (cpid > 0)
         exit(0);
 
-    pid_t sid = setsid();
+#ifdef SETSID_DAEMONIZATION
+    // pid_t sid = setsid();
     if (sid < 0)
     {
         log_error("%s(setsid(: %s\n", __func__, strerror(errno));
@@ -35,6 +36,6 @@ void daemonize(void)
 
     if (cpid > 0)
         exit(0);
-
+#endif /* SETSID_DAEMONIZATION */
     log_info("Started daemon process with pid %d\n", getpid());
 }
